f20ce99601d6a842b78d9e72a0f52dc81f33fe9b
gungor <gungor@kth.se>
2017-12-04 09:32:04 -0800

    SixEpSimulator improved

--- IoT_internal/SixEpSimulator.java
+++ IoT_internal/SixEpSimulator.java
@@ -1,3 +1,4 @@
+import java.io.ByteArrayOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -7,13 +8,24 @@ import java.nio.ByteBuffer;
 
 public class SixEpSimulator {
 
-	public static void main(String[] args) throws Exception {
-		DatagramSocket clientSocket=new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("localhost");
-		byte[] sendData = new byte[1024];
-		String sentence = "this_is_a_message_from_the_user_Hopefully_it_will_work_or_otherwise_I_will_not_tell_you_what_I_will_do";
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 5151);
-		clientSocket.send(sendPacket);
-	}
-}
+    public static void main(String[] args) throws Exception {
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int srcPort = 1515;
+        InetAddress srcAddr = InetAddress.getByName("2001:0DB8:AC10:FE01:0000:0000:0000:0000");
+        int dstPort = 1516;
+        InetAddress dstAddr = InetAddress.getByName("2001:0DB8:AC10:FE01:0000:0000:0000:0001");
+        String sentence = "Hello";
+        outputStream.write(srcAddr.getAddress());
+        outputStream.write((byte) srcPort & 0xFF);
+        outputStream.write((byte) (srcPort >> 8) & 0xFF);
+        outputStream.write(dstAddr.getAddress());
+        outputStream.write((byte) dstPort & 0xFF);
+        outputStream.write((byte) (dstPort >> 8) & 0xFF);
+        outputStream.write(sentence.getBytes());
+        byte out[] = outputStream.toByteArray();
+        DatagramPacket sendPacket = new DatagramPacket(out, out.length, IPAddress, 9000);
+        clientSocket.send(sendPacket);
+    }
+
