55fce3bcaf0a60edfc3ee1361cde3239730407ca
gungor <gungor@kth.se>
2017-12-11 14:27:59 -0800

    IEP implementation via tun/tap interface

---
+++ IoT_internal/iep_tun/ieptun.c
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2017, Copyright Arman Gungor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Author  : Arman Gungor gungor@kth.se
+ * Created : 2017-12-03
+ *
+ * tunproxy.c 6EP tunneling over UDP with tun/tap
+ * modified version of http://www.secdev.org/projects/tuntap_udp/files/tunproxy.c
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <getopt.h>
+#include <sys/ioctl.h>
+
+#include "nat64.h"
+
+#define PERROR(x) do { perror(x); exit(1); } while (0)
+#define ERROR(x, args ...) do { fprintf(stderr,"ERROR:" x, ## args); exit(1); } while (0)
+
+void usage()
+{
+	fprintf(stderr, "Usage: tunproxy [-s port|-c targetip:port] [-e]\n");
+	exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	struct sockaddr_in sin, sout, from;
+	struct ifreq ifr;
+	int fd, s, fromlen, soutlen, port, PORT, l, offset;
+	char c, *p, *ip;
+	char bufin[1504];
+	char bufout[1504];
+	int ethheaderlen = 14;
+	char ethheader[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+		                 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c,
+						 0x08, 0x00 };
+	fd_set fdset;
+	
+
+	int TUNMODE = IFF_TUN, DEBUG = 1;
+
+	//default settings
+	PORT = 9000;
+
+	while ((c = getopt(argc, argv, "s:c:ehd")) != -1) {
+		switch (c) {
+		case 'h':
+			usage();
+		case 'd':
+			DEBUG++;
+			break;
+		case 's':
+			PORT = atoi(optarg);
+			break;
+		case 'c':
+			p = memchr(optarg,':',16);
+			if (!p) ERROR("invalid argument : [%s]\n",optarg);
+			*p = 0;
+			ip = optarg;
+			port = atoi(p+1);
+			PORT = 0;
+			break;
+		case 'e':
+			TUNMODE = IFF_TAP;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	//create and initialize tun interface socket
+	if ( (fd = open("/dev/net/tun",O_RDWR)) < 0) PERROR("open");
+
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.ifr_flags = TUNMODE | IFF_NO_PI;
+	strncpy(ifr.ifr_name, "toto%d", IFNAMSIZ);
+	if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) PERROR("ioctl");
+
+	printf("Allocated interface %s. Configure and use it\n", ifr.ifr_name);
+	
+	//create and initialize udp tunnel socket to communicate with 6EP
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_port = htons(PORT);
+	if ( bind(s,(struct sockaddr *)&sin, sizeof(sin)) < 0) PERROR("bind");
+
+	fromlen = sizeof(from);
+
+	nat64_init();
+
+	offset = 0;
+	bzero(bufout, offset);
+	if(TUNMODE == IFF_TAP) {
+		memcpy(&bufout[offset], ethheader, ethheaderlen);
+		offset += ethheaderlen;
+
+	}
+
+	//receive first packet to establish tunnel
+	while(1) {
+		l = recvfrom(s, bufin, sizeof(bufin), 0, (struct sockaddr *)&from, &fromlen);
+		if (l < 0) PERROR("recvfrom");
+		break;
+	}
+
+	//recv packet from 6EP and send them to tun interface and vice versa.
+	while (1) {
+		FD_ZERO(&fdset);
+		//add tun interface and udp tunnel sockets to select set
+		FD_SET(fd, &fdset);
+		FD_SET(s, &fdset);
+		if (select(fd+s+1, &fdset,NULL,NULL,NULL) < 0) PERROR("select");
+		if (FD_ISSET(fd, &fdset)) {
+			//read packet from tun interface
+			if (DEBUG) write(1,">", 1);
+			l = read(fd, bufin, sizeof(bufin));
+			//packet received from internet translate to IPv6 packet
+			if (l < 0) PERROR("read");
+		    l = ip64_4to6(bufin, l, bufout);
+			if (sendto(s, bufout, l, 0, (struct sockaddr *)&from, fromlen) < 0) PERROR("sendto");
+		} else {
+			if (DEBUG) write(1,"<", 1);
+			l = recvfrom(s, &bufin, (sizeof(bufin)), 0, (struct sockaddr *)&sout, &soutlen);
+			//packet received from 6EP translate to IPv4 packet
+			l = ip64_6to4(bufin, l, &bufout[offset]);
+			//write resulting packet to tun interface
+			if (write(fd, bufout, l+offset) < 0) PERROR("write");
+		}
+	}
+}
+	       
+	
---
+++ IoT_internal/iep_tun/nat64.h
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2017, Copyright Arman Gungor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Author  : Arman Gungor gungor@kth.se
+ * Created : 2017-12-03
+ *
+ * nat64.h
+ * modified version of ip64 module in contiki
+ *
+ */
+
+#ifndef NAT64_H_
+#define NAT64_H_
+#define EPHEMERAL_PORTRANGE 1024
+
+#define IPV6_HDRLEN 40
+#define IPV4_HDRLEN 20
+
+#define IP_PROTO_ICMPV4  1
+#define IP_PROTO_TCP     6
+#define IP_PROTO_UDP     17
+#define IP_PROTO_ICMPV6  58
+
+#define ICMP_ECHO_REPLY  0
+#define ICMP_ECHO        8
+#define ICMP6_ECHO_REPLY 129
+#define ICMP6_ECHO       128
+
+#define BUFSIZE 600
+
+#define PRINTF(...) printf(__VA_ARGS__)
+
+#ifndef uip_ipaddr_copy
+#define uip_ipaddr_copy(dest, src) (*(dest) = *(src))
+#endif
+
+typedef unsigned char  boolean;
+typedef unsigned char  uint8_t;
+typedef signed   char  int8_t;
+typedef unsigned short uint16_t;
+typedef signed   short int16_t;
+typedef unsigned int   uint32_t;
+typedef signed   int   int32_t;
+typedef unsigned int   PointerType;
+
+/**
+ * Construct an IP address from four bytes.
+ *
+ * \param addr0 The first octet of the IP address.
+ * \param addr1 The second octet of the IP address.
+ * \param addr2 The third octet of the IP address.
+ * \param addr3 The forth octet of the IP address.
+ *
+ * \hideinitializer
+ */
+#define uip_ipaddr(addr, addr0,addr1,addr2,addr3) do {  \
+    (addr)->u8[0] = addr0;                              \
+    (addr)->u8[1] = addr1;                              \
+    (addr)->u8[2] = addr2;                              \
+    (addr)->u8[3] = addr3;                              \
+  } while(0)
+
+/**
+ * Construct an IPv6 address from eight 16-bit words.
+ *
+ * This function constructs an IPv6 address.
+ *
+ * \hideinitializer
+ */
+#define uip_ip6addr(addr, addr0,addr1,addr2,addr3,addr4,addr5,addr6,addr7) do { \
+    (addr)->u16[0] = htons(addr0);                                      \
+    (addr)->u16[1] = htons(addr1);                                      \
+    (addr)->u16[2] = htons(addr2);                                      \
+    (addr)->u16[3] = htons(addr3);                                      \
+    (addr)->u16[4] = htons(addr4);                                      \
+    (addr)->u16[5] = htons(addr5);                                      \
+    (addr)->u16[6] = htons(addr6);                                      \
+    (addr)->u16[7] = htons(addr7);                                      \
+  } while(0)
+
+#define uip_ip4addr_cmp(addr1, addr2) ((addr1)->u16[0] == (addr2)->u16[0] && \
+                                       (addr1)->u16[1] == (addr2)->u16[1])
+#define uip_ipaddr_maskcmp(addr1, addr2, mask)          \
+  (((((uint16_t *)addr1)[0] & ((uint16_t *)mask)[0]) ==       \
+    (((uint16_t *)addr2)[0] & ((uint16_t *)mask)[0])) &&      \
+   ((((uint16_t *)addr1)[1] & ((uint16_t *)mask)[1]) ==       \
+    (((uint16_t *)addr2)[1] & ((uint16_t *)mask)[1])))
+
+#define uip_ipaddr_prefixcmp(addr1, addr2, length) (memcmp(addr1, addr2, length>>3) == 0)
+
+/** \brief set IP address a to the link local all-nodes multicast address */
+#define uip_create_linklocal_allnodes_mcast(a) uip_ip6addr(a, 0xff02, 0, 0, 0, 0, 0, 0, 0x0001)
+
+/**
+ * Convert an IP address to four bytes separated by commas.
+ *
+ * Example:
+ \code
+ uip_ipaddr_t ipaddr;
+ printf("ipaddr=%d.%d.%d.%d\n", uip_ipaddr_to_quad(&ipaddr));
+ \endcode
+ *
+ * \param a A pointer to a uip_ipaddr_t.
+ * \hideinitializer
+ */
+#define uip_ipaddr_to_quad(a) (a)->u8[0],(a)->u8[1],(a)->u8[2],(a)->u8[3]
+
+struct tcp_hdr {
+  uint16_t srcport;
+  uint16_t destport;
+  uint8_t seqno[4];
+  uint8_t ackno[4];
+  uint8_t tcpoffset;
+  uint8_t flags;
+  uint8_t  wnd[2];
+  uint16_t tcpchksum;
+  uint8_t urgp[2];
+  uint8_t optdata[4];
+};
+
+struct udp_hdr {
+  uint16_t srcport;
+  uint16_t destport;
+  uint16_t udplen;
+  uint16_t udpchksum;
+};
+
+struct icmpv4_hdr {
+  uint8_t type, icode;
+  uint16_t icmpchksum;
+};
+
+struct icmpv6_hdr {
+  uint8_t type, icode;
+  uint16_t icmpchksum;
+  uint16_t id, seqno;
+};
+
+typedef union {
+  uint32_t u32[(BUFSIZE + 3) / 4];
+  uint8_t u8[BUFSIZE];
+} uip_buf_t;
+
+/**
+ * Representation of an IP address.
+ *
+ */
+typedef union uip_ip4addr_t {
+  uint8_t  u8[4];                       /* Initializer, must come first. */
+  uint16_t u16[2];
+} uip_ip4addr_t;
+
+typedef union uip_ip6addr_t {
+  uint8_t  u8[16];                      /* Initializer, must come first. */
+  uint16_t u16[8];
+} uip_ip6addr_t;
+
+struct ipv6_hdr {
+  uint8_t vtc;
+  uint8_t tcflow;
+  uint16_t flow;
+  uint8_t len[2];
+  uint8_t nxthdr, hoplim;
+  uip_ip6addr_t srcipaddr, destipaddr;
+};
+
+struct ipv4_hdr {
+  uint8_t vhl,
+    tos,
+    len[2],
+    ipid[2],
+    ipoffset[2],
+    ttl,
+    proto;
+  uint16_t ipchksum;
+  uip_ip4addr_t srcipaddr, destipaddr;
+};
+
+void ip64_init(void);
+int nat64_6to4(const uint8_t *ipv6packet, const uint16_t ipv6len,
+              uint8_t *resultpacket);
+
+#endif /* NAT64_H_ */
